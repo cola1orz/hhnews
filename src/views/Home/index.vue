b03cc2756b18aa7871a275f3a8f0d63122351889
cola1orz <924875523@qq.com>
2019-07-27 20:54:57 +0800

    结构完成

---
+++ src/views/Home/index.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+
+        <h4>HOME</h4>
+        <router-link to='/VuexDemo'>To VuexDemo</router-link>
+        <hr>
+        <router-link to='/login'>To login</router-link>
+    </div>
+</template>
+<script>
+export default {
+  name: 'HomeIndex'
+}
+</script>
---
+++ src/views/VuexDemo/VuexDemo.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="VuexDemo">
+  <p>{{count}}</p>
+  <button @click='increment'>+</button>
+  </div>
+</template>
+
+<script>
+import { mapState, mapMutations } from 'vuex'
+export default {
+  name: 'VuexDemo',
+  computed: {
+    ...mapState(['count'])
+  },
+  methods: {
+    ...mapMutations(['increment'])
+  }
+}
+</script>
